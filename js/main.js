106a14b520674d47e716b77c6f71136d7c01291c
kelvin algusto <kelvin.zuzor@gmail.com>
2020-10-18 16:52:33 -0300

    update fix bugs styles and css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,68 @@
-/*var novaURL = "../pages/login.html";
+var novaURL = "../pages/login.html";
 
- async function isloggedIn() {
-   
-    let user = {};
-    user = await sessionStorage.getItem("user");
-    console.log(JSON.parse(user))
+async function isloggedIn2() {
+
+  let user;
+
+  try {
+    user = await sessionStorage.getItem("user") //JSON.parse(sessionStorage.getItem("user"));
+  } catch (error) {
+    console.log(error)
+  }
 
 
-    if (user == null) {
-        $(window.document.location).attr('href', novaURL);
-    } else {
-      //  $(window.document.location).attr('href', "index.html");
-    }
- 
 };
 
-//window.onload = isloggedIn();
-*/
+function name() {
+  user = sessionStorage.getItem("user")
+  if (user == 1) {
+    console.log(user)
+    //$(window.document.location).attr('href', "../index.html");
+    window.location.href = "../index.html";
+  } else {
+
+    $(window.document.location).attr('href', novaURL);
+  }
+
+}
+window.onload = name();
+window.onload = console.log('alo')
+
+setTimeout(function () {
+  // isloggedIn();
+}, 500);
+
+$("#logout").on("click", () => {
+
+  sessionStorage.removeItem('user')
+  $(window.document.location).attr('href', "../pages/login.html")
+});
+
+
+(function ($) {
+  // USE STRICT
+  "use strict";
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 900,
+    outDuration: 900,
+    linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([class^="chosen-single"])',
+    loading: true,
+    loadingParentElement: 'html',
+    loadingClass: 'page-loader',
+    loadingInner: '<div class="page-loader__spin"></div>',
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: ['animation-duration', '-webkit-animation-duration'],
+    overlay: false,
+    overlayClass: 'animsition-overlay-slide',
+    overlayParentElement: 'html',
+    transition: function (url) {
+      window.location.href = url;
+    }
+  });
+
+
+})(jQuery);
